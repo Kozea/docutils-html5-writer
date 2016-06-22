0a8bd81be2dc316c015a91148fe3d07735c2aedd
Florian Mounier <florian.mounier@kozea.fr>
2016-06-22 10:28:39 +0200

    Just to be sure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='docutils_html5_writer',
-    version='0.1.0',
+    version='1.0.0',
     author='James H. Fisher, Kozea',
     license='public domain',
     install_requires=['docutils', 'html5lib'],
